545aae12ea144a6975340e296fdb2f258e5c10af
Zhabskyi <poltavagaz@gmail.com>
2019-12-04 15:30:07 -0700

    Add filter to panels in dashboard element

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import Loading from "./Loading";
-import Panel from './Panel';
+import Panel from "./Panel";
 
 import classnames from "classnames";
-import data from './data';
-
+import data from "./data";
 
 class Dashboard extends Component {
   state = {
-    loading: false
+    loading: false,
+    focused: null
   };
 
   render() {
@@ -18,16 +18,20 @@ class Dashboard extends Component {
       return <Loading />;
     }
 
-    return <main className={dashboardClasses} >
-      {data.map( ({id, label, value}) => {
-        return (
+    const panels = data
+      .filter(
+        panel => this.state.focused === null || this.state.focused === panel.id
+      )
+      .map(panel => (
         <Panel
-        id={id}
-        label={label}
-        value={value}
-         />)
-      })}
-       </main>;
+          key={panel.id}
+          id={panel.id}
+          label={panel.label}
+          value={panel.value}
+        />
+      ));
+
+    return <main className={dashboardClasses}>{panels}</main>;
   }
 }
 
