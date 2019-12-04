7b6f700ec4317366a165e74295214e350f0b4840
Zhabskyi <poltavagaz@gmail.com>
2019-12-04 15:26:40 -0700

    Init commit

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,33 @@
 import React, { Component } from "react";
+import Loading from "./Loading";
+import Panel from './Panel';
 
 import classnames from "classnames";
+import data from './data';
+
 
 class Dashboard extends Component {
+  state = {
+    loading: false
+  };
+
   render() {
     const dashboardClasses = classnames("dashboard");
 
-    return <main className={dashboardClasses} />;
+    if (this.state.loading) {
+      return <Loading />;
+    }
+
+    return <main className={dashboardClasses} >
+      {data.map( ({id, label, value}) => {
+        return (
+        <Panel
+        id={id}
+        label={label}
+        value={value}
+         />)
+      })}
+       </main>;
   }
 }
 
