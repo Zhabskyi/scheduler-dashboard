0bbb80ba22920138bf7de64009495ad83014201e
Zhabskyi <poltavagaz@gmail.com>
2019-12-04 18:31:33 -0700

    Add click event to panel

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,14 +6,36 @@ import classnames from "classnames";
 import data from "./data";
 
 class Dashboard extends Component {
+
   state = {
     loading: false,
     focused: null
   };
 
-  render() {
-    const dashboardClasses = classnames("dashboard");
+  componentDidMount() {
+    const focused = JSON.parse(localStorage.getItem("focused"));
 
+    if (focused) {
+      this.setState({ focused });
+    }
+  }
+
+  componentDidUpdate(previousProps, previousState) {
+    if (previousState.focused !== this.state.focused) {
+      localStorage.setItem("focused", JSON.stringify(this.state.focused));
+    }
+  }
+
+  selectPanel = id => {
+    this.setState(previousState => ({
+      focused: previousState.focused !== null ? null : id
+    }));
+  };
+
+  render() {
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
     if (this.state.loading) {
       return <Loading />;
     }
@@ -28,6 +50,7 @@ class Dashboard extends Component {
           id={panel.id}
           label={panel.label}
           value={panel.value}
+          onSelect={this.selectPanel}
         />
       ));
 
